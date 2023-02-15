6075a4e0d40bf3fe907c893382abee077d4acb94
kirikirimai <go.revolution.no3@gmail.com>
2023-02-15 18:11:06 +0900

    image change

--- src/pages/imagedisp.tsx
+++ src/pages/imagedisp.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Layout from '../componets/Layout'
+import {css} from "@emotion/react";
+
+import imgGatsby1 from "../images/img_gatsby1.jpg"
 
 const imageDisp = () => {
   return (
-    <div>imageDisp</div>
+    <Layout>
+    <h1>画像のテスト</h1>
+    <hr/>
+    <h2>import して画像を読み込む</h2>
+    <img css={imgWidthS} src={imgGatsby1} />
+
+    <hr/>
+    <h2>staticフォルダから呼び出す</h2>
+    <img src='/img_gatsby2.jpg' />
+
+    <hr/>
+    <h2>Gatsby Imageのプラグインを使用</h2>
+    <img css={imgWidthS} src={imgGatsby1} />
+    </Layout>
   )
 }
 
+const imgWidthS=css`
+width: 30%;
+`
 export default imageDisp
